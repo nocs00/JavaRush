1bad1caedf31c802a6e75e262d4d8367e2930086
Paul Dudenkov <nocs00@gmail.com>
2016-10-05 19:35:40 +0300

    lvl34 big end

--- src/main/java/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
+++ src/main/java/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
@@ -1,31 +1,70 @@
 package com.javarush.test.level34.lesson15.big01.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.nio.file.Path;
 import java.util.HashSet;
-import java.util.Set;
 
 public class LevelLoader {
 
-    public LevelLoader(Path levels) {
+    private Path levels;
 
+    public LevelLoader(Path levels) {
+        this.levels = levels;
     }
 
     public GameObjects getLevel(int level) {
-        int centralCoord = Model.FIELD_SELL_SIZE/2;
-        Set<Wall> walls = new HashSet<>();
-        walls.add(new Wall(4*centralCoord, 4*centralCoord));
-        walls.add(new Wall(10*centralCoord, 10*centralCoord));
-        walls.add(new Wall(16*centralCoord, 16*centralCoord));
-        Set<Box> boxes = new HashSet<>();
-        boxes.add(new Box(20*centralCoord, 20*centralCoord));
-        boxes.add(new Box(30*centralCoord, 20*centralCoord));
-        boxes.add(new Box(20*centralCoord, 30*centralCoord));
-        Set<Home> homes = new HashSet<>();
-        homes.add(new Home(26*centralCoord, 26*centralCoord));
-        homes.add(new Home(20*centralCoord, 26*centralCoord));
-        Player player = new Player(30*centralCoord, 30*centralCoord);
+        level = level & 61;
 
-        GameObjects gameObjects = new GameObjects(walls, boxes, homes, player);
-        return gameObjects;
+        HashSet<Wall> walls = new HashSet<>();
+        HashSet<Box> boxes = new HashSet<>();
+        HashSet<Home> homes = new HashSet<>();
+        Player player = null;
+
+        try (
+                BufferedReader fileReader = new BufferedReader(new FileReader(levels.toString()))
+                ) {
+
+            int base = Model.FIELD_SELL_SIZE;
+            int offset = base / 2;
+
+            while (!fileReader.readLine().contains("Maze: " + level)) ;
+            for (int i = 0; i < 2; i++) fileReader.readLine();
+            int levelHeight = Integer.parseInt(fileReader.readLine().split(" ")[2]);
+            for (int i = 0; i < 3; i++) fileReader.readLine();
+
+            for (int _y = 0; _y < levelHeight; _y++) {
+                String read = fileReader.readLine();
+                for (int _x = 0; _x < read.length(); _x++) {
+
+                    int x = _x * base + offset;
+                    int y = _y * base + offset;
+
+                    switch (read.charAt(_x)) {
+                        case 'X':
+                            walls.add(new Wall(x, y));
+                            break;
+                        case '@':
+                            player = new Player(x, y);
+                            break;
+                        case '*':
+                            boxes.add(new Box(x, y));
+                            break;
+                        case '.':
+                            homes.add(new Home(x, y));
+                            break;
+                        case '&':
+                            boxes.add(new Box(x, y));
+                            homes.add(new Home(x, y));
+                            break;
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return new GameObjects(walls, boxes, homes, player);
     }
 }
