4509e871e7049d20dafd4e0ffb7add83fc8d39cd
Paul <nocs00@gmail.com>
2016-10-24 23:55:31 +0300

    level38

--- src/main/java/com/javarush/test/level36/lesson08/bonus01/Solution.java
+++ src/main/java/com/javarush/test/level36/lesson08/bonus01/Solution.java
@@ -9,5 +9,17 @@ package com.javarush.test.level36.lesson08.bonus01;
 */
 public class Solution {
     public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+
+        System.out.println(tree.isEmpty());
+
+        tree.insert(1);
+        tree.insert(2);
+        tree.insert(3);
+        tree.insert(4);
+        tree.insert(5);
+
+        System.out.println(tree.isEmpty());
+
     }
 }
---
+++ src/main/java/com/javarush/test/level38/lesson10/home02/Date.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level38.lesson10.home02;
+
+public @interface Date {
+    //напиши свой код
+    int month();
+
+    int year();
+
+    int day();
+
+    int second();
+
+    int minute();
+
+    int hour();
+}
